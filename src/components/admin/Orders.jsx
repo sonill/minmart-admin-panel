1922c5040e8f19b5a9bafdcd36961a1a82a72760
Yogendra Rana <yogendrarana4321@gmail.com>
2023-06-06 13:52:22 +0545

    Some fixes

--- src/components/admin/Orders.jsx
+++ src/components/admin/Orders.jsx
@@ -20,7 +20,7 @@ const Orders = () => {
 
     return (
         <div className='w-full flex flex-col border rounded-[0.5rem] overflow-hidden'>
-            <table className="w-[100%] border-spacing-0">
+            <table className="w-[100%]">
                 <thead>
                     <tr className="bg-[#ededed]">
                         <th className="p-[1.5rem] text-justify text-[1.45rem]">User ID</th>
@@ -38,7 +38,7 @@ const Orders = () => {
                                 <tr key={index} className="border-b last:border-0">
                                     <td className='p-[1.5rem] text-[1.35rem]'>{order?.user_id}</td>
                                     <td aria-hidden className="p-[1.5rem] text-[1.35rem]">
-                                        <span className={`rounded-full px-[0.75rem] text-[1rem] py-[0.25rem] text-white ${order.status === "pending" ? "bg-red-500" : "bg-green-500"}`}>{order.status}</span>
+                                        <span className={`rounded-full px-[0.75rem] text-[1rem] py-[0.25rem] text-white ${order.status === "pending" ? "bg-red-400" : "bg-green-400"}`}>{order.status}</span>
                                     </td>
                                     <td className='p-[1.5rem] text-[1.35rem]'>{order.total}</td>
                                     <td className='p-[1.5rem] text-[1.35rem]'>{moment(order.date.seconds * 1000).format('YYYY-MM-DD')}</td>
