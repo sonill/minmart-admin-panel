cb2b4fa41ac90415c68896bf0c204b8a692d7e89
Yogendra Rana <yogendrarana4321@gmail.com>
2023-06-09 14:07:12 +0545

    Excel data upload to Products collection completed

--- src/components/admin/Products.jsx
+++ src/components/admin/Products.jsx
@@ -1,59 +1,93 @@
-import { read, utils } from 'xlsx';
-import { useRef, useState } from 'react'
+import { read, utils, writeFile } from 'xlsx';
+import { useEffect, useRef, useState } from 'react'
 import { toast } from 'react-hot-toast';
-import { addDoc, collection, deleteDoc, doc, getDocs, setDoc } from 'firebase/firestore';
+import { addDoc, collection, deleteDoc, getDocs } from 'firebase/firestore';
 import { db } from '../../firebase';
 
 const Products = () => {
     const [keyword, setKeyword] = useState("");
-    const [file, setFile] = useState('');
     const nameRef = useRef(null);
-    const [tableData, setTableData] = useState([]);
+    const [productsData, setProductsData] = useState([]);
+    const [excelData, setExcelData] = useState([]);
 
-
+    // handle file change
     async function handleFileChange(e) {
         e.preventDefault();
-        setFile(e.target.files[0]);
         nameRef.current.innerText = e.target.files[0].name;
 
         // xslx 
         const f = await e.target.files[0].arrayBuffer();
         const wb = read(f);
         const data = utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]);
-        setTableData(data);
+
+        setExcelData(data);
     }
 
+    // handle file remove
     function handleFileRemove() {
-        setFile('');
+        setExcelData([]);
         nameRef.current.innerText = '';
         const input = document.getElementById('input_field');
         if (input) { input.value = ''; }
     }
 
-    const collectionRef = collection(db, "dummycollection");
-
+    // handle save
     async function handleSave() {
         const toastId = toast.loading("Uploading products...");
-    
+
         try {
-            const querySnapshot = await getDocs(collectionRef);
-            querySnapshot.forEach((doc) => {
+            let items = {};
+
+            excelData.forEach((data) => {
+                const variations = [];
+
+                Object.keys(data).forEach((key) => {
+                    if (key !== "name" && data[key] !== "null") {
+                        variations.push({ label: key, price: data[key] });
+                    }
+                });
+
+                items[data.name] = { name: data.name, variations: variations };
+            });
+
+            const querySnapshot = await getDocs(collection(db, "products"));
+
+            querySnapshot.docs.forEach((doc) => {
                 deleteDoc(doc.ref);
             });
 
-            for (const item of tableData) {
-                await addDoc(collectionRef, item);
-            }
+            await addDoc(collection(db, "products"), {items: items});
 
-            toast.success("Data saved successfully!", {id: toastId});
+            toast.success("Data saved successfully!", { id: toastId });
 
             handleFileRemove();
         } catch (error) {
-            toast.error(error.message, {id: toastId});
+            toast.error(error.message, { id: toastId });
         }
     }
 
+    useEffect(() => {
+        const fetchCollection = async () => {
+            try {
+                const querySnapshot = await getDocs(collection(db, "products"));
+                const data = querySnapshot.docs.map((doc) => doc.data());
 
+                if(data.length){
+                    const products = Object.keys(data[0].items).map(key => {
+                        const product = data[0].items[key];
+                        return { name: product.name, variations: product.variations };
+                    })
+
+                    setProductsData(products);
+                }
+
+            } catch (error) {
+                console.error('Error fetching collection:', error);
+            }
+        };
+
+        fetchCollection();
+    }, []);
 
 
     return (
@@ -64,16 +98,14 @@ const Products = () => {
                     <i className="fa-solid fa-magnifying-glass absolute cursor-pointer text-[#7d817e] text-[1.5rem] bottom-[0.5rem] right-[1rem] transform translate-y-[-50%]"></i>
                 </div>
 
-                <div className={`${file == '' ? 'hidden' : 'flex'} mr-[1rem] custom-shadow flex gap-[1rem] items-center text-[1.25rem] duration-150 text-[#b2eb5] px-[2rem] py-[1rem] rounded-[0.5rem]`}>
-                    <i onClick={handleFileRemove} className="fa-solid fa-xmark text-[1.5rem] cursor-pointer"></i>
-                    <p ref={nameRef}></p>
+                <div className={`${excelData.length == 0 ? 'hidden' : 'flex'} mr-[1rem] custom-shadow flex justify-between gap-[1rem] items-center text-[1.25rem] duration-150 text-[#b2eb5] rounded-[0.5rem]`}>
+                    <p ref={nameRef} className='pl-[1rem]'></p>
+                    <i onClick={handleFileRemove} className="fa-solid fa-xmark h-full text-[1rem] border cursor-pointer grid place-items-center px-[1.25rem] bg-gray-200 hover:bg-slate-300 duration-200"></i>
                 </div>
 
-                {file !== '' && <button onClick={handleFileRemove} className=' bg-[#27374D] text-white mr-[1rem] custom-shadow flex items-center text-[1.25rem] duration-150 text-[#b2eb5] px-[2rem] py-[1rem] rounded-[0.5rem]'>Cancel</button>}
+                {excelData.length !== 0 && <button onClick={handleSave} className='text-white bg-blue-500 hover:bg-blue-600 mr-[1rem] custom-shadow flex items-center text-[1.25rem] duration-150 text-[#b2eb5] px-[2rem] py-[1rem] rounded-[0.5rem]'>Save</button>}
 
-                {file !== '' && <button onClick={handleSave} className='text-white bg-blue-500 hover:bg-blue-600 mr-[1rem] custom-shadow flex items-center text-[1.25rem] duration-150 text-[#b2eb5] px-[2rem] py-[1rem] rounded-[0.5rem]'>Save</button>}
-
-                {file === '' && <label htmlFor='input_field' className='cursor-pointer h-[4rem] flex items-center text-[1.25rem] bg-blue-500 hover:bg-blue-600 duration-150 text-[#fff] px-[2rem] py-[1rem] rounded-[0.5rem] shadow-lg'>
+                {excelData.length === 0 && <label htmlFor='input_field' className='cursor-pointer h-[4rem] flex items-center text-[1.25rem] bg-blue-500 hover:bg-blue-800 duration-150 text-[#fff] px-[2rem] py-[1rem] rounded-[0.5rem] shadow-lg'>
                     <i className="fa-solid fa-upload text-white"></i>
                     <p className='ml-[1rem] text-white'>Add Product</p>
                     <input type="file" accept=".xlsx, .xls, application/vnd.ms-excel" className='hidden' id='input_field' onChange={handleFileChange} />
