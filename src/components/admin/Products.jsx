8b22f3a0e8ad724dce1a021580a8983c4cf18172
Yogendra Rana <yogendrarana4321@gmail.com>
2023-06-12 11:21:08 +0545

    Products filtering functionnality added

--- src/components/admin/Products.jsx
+++ src/components/admin/Products.jsx
@@ -1,4 +1,4 @@
-import { read, utils, writeFile } from 'xlsx';
+import { read, utils } from 'xlsx';
 import { useEffect, useRef, useState } from 'react'
 import { toast } from 'react-hot-toast';
 import { addDoc, collection, deleteDoc, getDocs } from 'firebase/firestore';
@@ -9,6 +9,7 @@ const Products = () => {
     const nameRef = useRef(null);
     const [productsData, setProductsData] = useState([]);
     const [excelData, setExcelData] = useState([]);
+    const [selectedVariation, setSelectedVariation] = useState("kg");
 
     // handle file change
     async function handleFileChange(e) {
@@ -40,9 +41,8 @@ const Products = () => {
 
             excelData.forEach((data) => {
                 const variations = [];
-
                 Object.keys(data).forEach((key) => {
-                    if (key !== "name" && data[key] !== "null") {
+                    if (key !== "name") { //use this to filter out null fields: data[key] !== "null"
                         variations.push({ label: key, price: data[key] });
                     }
                 });
@@ -56,47 +56,53 @@ const Products = () => {
                 deleteDoc(doc.ref);
             });
 
-            await addDoc(collection(db, "products"), {items: items});
-
+            await addDoc(collection(db, "products"), { items: items });
+            fetchProducts();
             toast.success("Data saved successfully!", { id: toastId });
-
             handleFileRemove();
         } catch (error) {
             toast.error(error.message, { id: toastId });
         }
     }
 
-    useEffect(() => {
-        const fetchCollection = async () => {
-            try {
-                const querySnapshot = await getDocs(collection(db, "products"));
-                const data = querySnapshot.docs.map((doc) => doc.data());
+    // handle variants change
+    const handleVariationChange = (e) => {
+        setSelectedVariation(e.target.value);
+    };
 
-                if(data.length){
-                    const products = Object.keys(data[0].items).map(key => {
-                        const product = data[0].items[key];
-                        return { name: product.name, variations: product.variations };
-                    })
+    // fetch products
+    const fetchProducts = async () => {
+        try {
+            const querySnapshot = await getDocs(collection(db, "products"));
+            const data = querySnapshot.docs.map((doc) => doc.data())
 
-                    setProductsData(products);
-                }
+            if (data.length) {
+                const productsArray = Object.keys(data[0].items).map(key => {
+                    const product = data[0].items[key];
+                    return { name: product.name, variations: product.variations };
+                })
 
-            } catch (error) {
-                console.error('Error fetching collection:', error);
+                const products = productsArray.filter((prod) => prod.name.includes(keyword));
+                setProductsData(products);
             }
-        };
 
-        fetchCollection();
-    }, []);
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
+
+    useEffect(() => {
+        fetchProducts();
+    }, [keyword]);
 
 
     return (
         <div className='flex flex-col'>
             <div className='w-[100%] flex pb-[1.5rem]'>
                 <div className='mr-auto relative flex items-center text-[1.25rem] rounded-[0.5rem]'>
-                    <input placeholder="Quick search" className='h-[4rem] border border-gray-300 p-[0.75rem] rounded-[0.5rem] text-[1.5rem] pr-[5rem]' value={keyword} onChange={e => setKeyword(e.target.value)} />
-                    <i className="fa-solid fa-magnifying-glass absolute cursor-pointer text-[#7d817e] text-[1.5rem] bottom-[0.5rem] right-[1rem] transform translate-y-[-50%]"></i>
-                </div>
+                    <input placeholder="Quick search" value={keyword} onChange={e => setKeyword(e.target.value)} className='h-[4rem] border border-gray-300 p-[0.75rem] rounded-[0.5rem] text-[1.5rem] pr-[5rem]' />
+                    {!keyword && <i className="fa-solid fa-magnifying-glass absolute cursor-pointer text-[#7d817e] text-[1.5rem] bottom-[0.5rem] right-[1rem] transform translate-y-[-50%]"></i>}
+                    {keyword && <i onClick={() => setKeyword('')} className="fa-solid fa-xmark absolute cursor-pointer text-[#7d817e] text-[1.5rem] bottom-[0.5rem] right-[1rem] transform translate-y-[-50%]"></i>}                </div>
 
                 <div className={`${excelData.length == 0 ? 'hidden' : 'flex'} mr-[1rem] custom-shadow flex justify-between gap-[1rem] items-center text-[1.25rem] duration-150 text-[#b2eb5] rounded-[0.5rem]`}>
                     <p ref={nameRef} className='pl-[1rem]'></p>
@@ -116,27 +122,35 @@ const Products = () => {
             <table className="table-auto w-[100%] rounded-[0.5rem] overflow-hidden custom-shadow">
                 <thead>
                     <tr className="bg-[#ededed]">
-                        <th className="p-[1.5rem] text-justify text-[1.45rem]">User ID</th>
-                        <th className="p-[1.5rem] text-justify text-[1.45rem]">Status</th>
-                        <th className="p-[1.5rem] text-justify text-[1.45rem]">Total</th>
-                        <th className="p-[1.5rem] text-justify text-[1.45rem]">Date</th>
+                        <th className="p-[1.5rem] text-justify text-[1.45rem]">Name</th>
+                        <th className="p-[1.5rem] text-justify text-[1.45rem]">Variations</th>
+                        <th className="p-[1.5rem] text-justify text-[1.45rem]">Price (Rs)</th>
                         {/* <th className="p-[1.5rem] text-justify text-[1.45rem]">Actions</th> */}
                     </tr>
                 </thead>
 
                 <tbody>
                     {
-                        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((_, index) => {
+                        productsData && productsData.map((data, index) => {
                             return (
-                                <tr key={index} className="border-b hover:bg-gray-100 duration-150">
-                                    <td className='p-[1.5rem] text-[1.35rem]'>asdfasdf</td>
-                                    <td className="p-[1.5rem] text-[1.35rem]">asdfasdf</td>
-                                    <td className='p-[1.5rem] text-[1.35rem]'>asdfasdf</td>
-                                    <td className='p-[1.5rem] text-[1.35rem]'>asdfasdf</td>
-                                    {/* <td className='p-[1.5rem] text-[1.35rem]'>
-                                        <i className="fa-solid fa-pencil mr-[1rem] cursor-pointer"></i>
-                                        <i className="fa-solid fa-trash cursor-pointer"></i>
-                                    </td> */}
+                                <tr key={index} className="border-b">
+                                    <td className='p-[1.5rem] text-[1.35rem]'>{data.name}</td>
+                                    <td className='p-[1.5rem] text-[1.35rem]'>
+                                        <select className='bg-white cursor-pointer' value={selectedVariation} onChange={handleVariationChange}>
+                                            {data.variations.map((variation) => (
+                                                <option key={variation.label} value={variation.label} disabled={variation.price === "null"} className={`${variation.price === "null" ? "text-gray-300" : ""}`}>
+                                                    {variation.label}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </td>
+                                    <td className='p-[1.5rem] text-[1.35rem]'>
+                                        {selectedVariation &&
+                                            (data.variations.find((variation) => variation.label === selectedVariation)?.price !== "null" ?
+                                                data.variations.find((variation) => variation.label === selectedVariation)?.price :
+                                                "-")
+                                        }
+                                    </td>
                                 </tr>
                             );
                         })
